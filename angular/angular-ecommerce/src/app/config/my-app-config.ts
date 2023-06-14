4ad7a511e6403f37f9958bd194be5b4dde973489
jana <jantschick3@gmail.com>
2023-06-14 19:25:53 +0300

    angular

---
+++ angular/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa948fu0iT8M5uZb5d7',
+        issuer: 'https://dev-73350167.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
---
+++ angular/angular-ecommerce/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    luv2shopApiUrl: 'https://localhost:8443/api',
+    stripePublishableKey: 'pk_test_51N5Uz3CdTMp13ntLfZiH3PfJ7C5k4RmCLPLcO2I7QSePCyF0jhSIexsTo05bjiyeTq3EzN5lP7kWMekO1dSMWSz900fjDiommq'
+  };
